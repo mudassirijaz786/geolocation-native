59c4597aea61b09b688d212fd754b9d16c8d04c8
Sandil9 <sundiljutt618@gmail.com>
2019-11-04 20:15:37 +0500

    geolocation, current location, watching === slow response

---
+++ App.js
@@ -0,0 +1,38 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import Current from "./src/components/Current"
+import Watch from "./src/components/Watch"
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App: () => React$Node = () => {
+  return (
+  <View>
+      <Current/>
+      <Watch/>
+  </View>
+  );
+};
+
+export default App;
---
+++ src/components/Watch.js
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+import Geolocation from '@react-native-community/geolocation';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Dimensions,
+  Text,
+  AppRegistry,
+  StatusBar,
+} from 'react-native';
+class Watch extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      latitude: null,
+      longitude: null,
+      error:null,
+    };
+  }
+
+  componentDidMount() {
+    Geolocation.watchPosition(
+       (position) => {
+         console.log("watch position properties");
+         console.log(position);
+         this.setState({
+           latitude: position.coords.latitude,
+           longitude: position.coords.longitude,
+           error: null,
+         });
+       },
+       (error) => this.setState({ error: error.message }),
+       { enableHighAccuracy: true, timeout: 200000, maximumAge: 1000, distanceFilter: 3 },
+       
+     );
+   }
+
+
+  render() {
+    return (
+      <View>
+        <Text>Watching</Text>
+        <Text> Latitude {this.state.latitude} </Text>
+        <Text> Longitude {this.state.longitude} </Text>
+        <Text> {this.state.error} </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+});
+
+export default Watch;
